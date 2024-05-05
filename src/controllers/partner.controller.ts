e98d48e5a354e95c69783a33c4409ce4b1dbb50f
INGENIO <ingeniokhalif@gmail.com>
2024-05-05 23:55:40 +0100

    logic to create and get partners

---
+++ src/controllers/partner.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { Service } from "typedi";
+import PartnerService from "../services/partner.services";
+import { CreatePartnerInput } from "../schemas/partner.schemas";
+import HTTP from "../utils/constants/http.responses";
+
+@Service()
+export default class PartnerController {
+  constructor(private service: PartnerService) {}
+
+  async getPartners(req: Request, res: Response) {
+    const partners = await this.service.getPartners();
+
+    return res.status(HTTP.OK).json(partners);
+  }
+
+  async createPartner(
+    req: Request<{}, {}, CreatePartnerInput["body"]>,
+    res: Response
+  ) {
+    const partner = await this.service.createPartner(req.body);
+
+    return res.status(HTTP.CREATED).json(partner);
+  }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Express, Request, Response } from "express";
 import SmsRouter from "./routes/sms.routes";
 import AppsRouter from "./routes/apps.routes";
+import PartnersRouter from "./routes/partner.routes";
 
 export default function router(server: Express) {
   // Healthcheck endpoint
@@ -10,5 +11,6 @@ export default function router(server: Express) {
 
   //   API endpoints
   server.use("/api/apps", AppsRouter);
+  server.use("/api/partners", PartnersRouter);
   server.use("/api/sms", SmsRouter);
 }
