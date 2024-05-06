3dbe71e627d289cf9d403c25d63ba93b9fd801da
INGENIO <ingeniokhalif@gmail.com>
2024-05-06 16:25:44 +0100

    ensure name is unique when creating & updating app & partner

--- src/services/partner.services.ts
+++ src/services/partner.services.ts
@@ -1,6 +1,10 @@
 import { Service } from "typedi";
 import PartnerRepository from "../repositories/partner.repository";
 import { CreatePartnerInput } from "../schemas/partner.schemas";
+import { PartnerDocument } from "../models/partner.model";
+import ApiError from "../utils/errors/errors.base";
+import COMMON_MSG from "../utils/constants/common.msgs";
+import HTTP from "../utils/constants/http.responses";
 
 @Service()
 export default class PartnerService {
@@ -11,6 +15,30 @@ export default class PartnerService {
   }
 
   async createPartner(partner: CreatePartnerInput["body"]) {
+    const nameIsUnique = await this.nameIsUnique({ name: partner.name });
+
+    if (!nameIsUnique) {
+      throw new ApiError(COMMON_MSG.inUse("Name"), HTTP.BAD_REQUEST);
+    }
+
     return await this.repository.createPartner(partner);
+  }
+
+  async getPartner({ partnerId, name }: { partnerId?: string; name?: string }) {
+    return await this.repository.getPartner({ partnerId, name });
+  }
+
+  private async nameIsUnique({
+    partnerId,
+    name,
+  }: {
+    partnerId?: string;
+    name: string;
+  }) {
+    const partner = (await this.getPartner({ name })) as PartnerDocument;
+
+    if (!partnerId && partner) return false;
+
+    return partner._id.toString() === partnerId;
   }
 }
