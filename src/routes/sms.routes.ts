13cd7b511840d8110ddc886e2e9f3703b9f6fce0
INGENIO <ingeniokhalif@gmail.com>
2024-05-06 10:33:30 +0100

    linking isAllowedService to the sms service

--- src/routes/sms.routes.ts
+++ src/routes/sms.routes.ts
@@ -7,11 +7,13 @@ import Container from "typedi";
 import SmsController from "../controllers/sms.controller";
 import { tryCatch } from "../utils/errors/errors.utils";
 import isTrustedApp from "../middlewares/isTrustedApp";
+import isAllowedService from "../middlewares/isAllowedService";
 
 const SmsRouter = Router();
 const controller = Container.get(SmsController);
 
 SmsRouter.use(isTrustedApp);
+SmsRouter.use(isAllowedService);
 
 SmsRouter.post(
   "/send",
